9a524299963e6441b51fb190ea2c58ac96f0edd8
bheard <bheard@enssat.fr>
2024-10-08 22:36:50 +0200

    Fix Ball s'envoit en l'air

--- Assets/BallInteraction.cs
+++ Assets/BallInteraction.cs
@@ -1,27 +1,36 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.XR.Interaction.Toolkit;
+
 public class BallInteraction : MonoBehaviour
 {
-    public GameObject bounceBall;
-    // Start is called before the first frame update
+    public Rigidbody bounceBall; // L'objet auquel appliquer la force
+    public float upwardForce = 500f; // La force à appliquer vers le haut
+
     void Start()
     {
         XRGrabInteractable grabbable = GetComponent<XRGrabInteractable>();
+
+        // Ajouter un listener à l'événement activated pour lancer l'interaction
         grabbable.activated.AddListener(Interaction);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 
-    public void Interaction(ActivateEventArgs args)
+    // Méthode appelée lors de l'activation de l'objet
+    void Interaction(ActivateEventArgs args)
     {
-        GameObject otherBall = Instantiate(bounceBall);
-        otherBall.transform.position = transform.position;
-        otherBall.GetComponent<Rigidbody>().AddForce(transform.up * 10);
+        if (bounceBall != null)
+        {
+            // Applique une force vers le haut à l'objet cible
+            bounceBall.AddForce(Vector3.up * upwardForce);
+        }
+        else
+        {
+            Debug.LogWarning("Aucun objet cible n'a été assigné !");
+        }
     }
 }
