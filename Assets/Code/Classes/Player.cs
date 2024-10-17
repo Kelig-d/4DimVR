d0791aa40bfc379c412b5a0a8fd7f39561f79bf0
Kelig <kelig.villalard@gmail.com>
2024-10-17 15:04:13 +0200

    Organization + player object

---
+++ Assets/Code/Classes/Player.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.XR.CoreUtils;
+using UnityEngine;
+using UnityEngine.XR.Interaction.Toolkit;
+
+public class Player : MonoBehaviour
+{
+    public string playerName = "Player";
+    
+    public float health = 100f;
+    public float speed = 2f;
+    public float damage = 1f;
+    public bool rightHanded = true;
+    private bool dead = false;
+    public XROrigin xOrigin;
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (health <= 0)
+        {
+            dead = true;
+        }
+        if (xOrigin.transform.position.y < -10)
+        {
+            xOrigin.transform.position = new Vector3(0, 5, 0);
+        }
+    }
+    
+    
+}
