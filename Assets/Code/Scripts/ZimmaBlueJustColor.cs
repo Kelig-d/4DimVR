c0adcaff89b4a75aed81178020bc0bae29dd2227
Maeva <maeva.desab@gmail.com>
2024-12-04 20:13:52 +0100

    fix ZimmaBlue

--- Assets/Code/Scripts/ZimmaBlueJustColor.cs
+++ Assets/Code/Scripts/ZimmaBlueJustColor.cs
@@ -5,17 +5,13 @@ using UnityEngine.InputSystem;
 using UnityEngine;
 using Unity;
 
-public class EffetZimmaBlue : MonoBehaviour
+public class ZimmaBlueJustColor : MonoBehaviour
 {
     [SerializeField]
     private Material myMaterial;
 
     private Rigidbody myRigidbody;
     private BoxCollider myObject;
-    public float targetMin = 0.5f;
-    public float targetHeight = 5f;  // Hauteur cible à laquelle le cube doit monter
-    public float moveSpeed = 1f; 
-    private Vector3 originalScale; // Nouvelle taille lorsqu'il est violet
 
     private float timeColor;
 
@@ -35,14 +31,13 @@ public class EffetZimmaBlue : MonoBehaviour
         myMaterial.SetFloat("_ShaderX",0.0f);
         myObject = GetComponent<BoxCollider>();
         Renderer renderer = GetComponent<Renderer>();
-        originalScale = transform.localScale;
+
 
     }
 
     // Update is called once per frame
     void Update()
     {
-        EffetColor();
         RetourNormalColor();
     }
 
@@ -70,46 +65,15 @@ public class EffetZimmaBlue : MonoBehaviour
         }
     }
 
-    public void EffetColor(){
-        if (myMaterial.GetFloat("_ShaderX") == 0.75f){
-            if (myMaterial.color.ToString() == colorBlue.ToString()){
-                // Déplace le cube progressivement vers le BAS en fonction de la vitesse
-                float step = moveSpeed * Time.deltaTime;
-                transform.position = new Vector3(transform.position.x, Mathf.MoveTowards(transform.position.y, targetMin, step), transform.position.z);
-        
-            }else if(myMaterial.color.ToString() == colorYellow.ToString() && transform.position.y < targetHeight){
-                // Déplace le cube progressivement vers le haut en fonction de la vitesse
-                float step = moveSpeed * Time.deltaTime;
-                transform.position = new Vector3(transform.position.x, Mathf.MoveTowards(transform.position.y, targetHeight, step), transform.position.z);
-                
-            }else if(myMaterial.color.ToString() == colorPurple.ToString()){
-                // Change la taille du cube
-                transform.localScale = Vector3.Lerp(transform.localScale, originalScale / 2f, Time.deltaTime * 2f);   
-            }
-            else if(myMaterial.color.ToString() == colorWhite.ToString()){
-                GetComponent<Renderer>().enabled = false;  // Cache le cube
-            }
-        }
-    }
-
     //La couleur de l'objet redevient à sa couleur d'orignie en fonction du temps qui passe
     public void RetourNormalColor(){
         if( timeColor == 30.0f ){
-            GetComponent<Renderer>().enabled = true;
-            //myObject.enabled=true;
             myMaterial.SetFloat("_ShaderX",0.5f);
         }else if (myMaterial.GetFloat("_ShaderX") == 0.5f && timeColor == 15.0f ){
             myMaterial.SetFloat("_ShaderX",0.25f);
         }else if (myMaterial.GetFloat("_ShaderX") == 0.25f && timeColor == 0.0f ){
             myMaterial.SetFloat("_ShaderX",0.0f);
-            ResetSize();
         }
-    }
-
-    private void ResetSize()
-    {
-        // Réinitialise la taille du cube à sa taille d'origine
-        transform.localScale = originalScale;
     }
   
     IEnumerator timerColor(){
