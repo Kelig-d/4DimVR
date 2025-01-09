5f033c75f622a1a8d0c51147a5d7631d84598bf5
Kelig <kelig.villalard@gmail.com>
2025-01-09 10:41:00 +0100

    fixing door problem

--- Assets/Code/Scripts/PorteTeleportation.cs
+++ Assets/Code/Scripts/PorteTeleportation.cs
@@ -10,11 +10,11 @@ public class PorteTeleportation : MonoBehaviour
 
     void OnCollisionEnter(Collision collision)
     {
-        if (otherPortal.transform.parent.transform.localScale != transform.parent.transform.localScale)
-        {
-            collision.gameObject.transform.parent.transform.localScale *= otherPortal.transform.parent.transform.localScale.x / transform.parent.transform.localScale.x;
-            Time.timeScale = 0;
-        }
+        // if (otherPortal.transform.parent.transform.localScale != transform.parent.transform.localScale)
+        // {
+        //     collision.gameObject.transform.parent.transform.localScale *= otherPortal.transform.parent.transform.localScale.x / transform.parent.transform.localScale.x;
+        //     Time.timeScale = 0;
+        // }
         collision.gameObject.transform.position = new Vector3(
             otherPortal.transform.position.x + Mathf.Clamp(otherPortal.transform.parent.rotation.y,0,1.5f),
             otherPortal.transform.position.y-1.5f,
