2a470ee31302ed0a84ac77ecaa24f4133bbbef7f
Sipores <nathansans2610@gmail.com>
2025-02-01 19:05:09 +0100

    SpawnPoint

--- Assets/Code/Scripts/NewSpawnPoint.cs
+++ Assets/Code/Scripts/NewSpawnPoint.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class NewSpawnPoint : MonoBehaviour
@@ -8,17 +9,32 @@ public class NewSpawnPoint : MonoBehaviour
     // Start is called before the first frame update
     public GameObject spawnPoint;
     public bool Menu = false;
+
     private void OnTriggerEnter(Collider other)
     {
+        Debug.Log("Collision détectée avec : " + other.gameObject.name);
 
-        Player var = other.gameObject.GetComponent<Player>();
-        
-        if(var != null)
+        Transform parentTransform = other.gameObject.transform.parent;
+
+        if (parentTransform != null)
         {
-            var.ChangeSpawnPoint(spawnPoint, Menu);
-            Console.WriteLine("change Spawn");
+            Player player = parentTransform.GetComponent<Player>();
+
+            if (player != null)
+            {
+                Debug.Log("try change Spawn");
+                player.ChangeSpawnPoint(spawnPoint, Menu);
+            }
+            else
+            {
+                Debug.Log("no change Spawn");
+            }
         }
-
-
+        else
+        {
+            Debug.Log("Pas de parent, impossible de trouver Player.");
+        }
     }
+
+
 }
